136322a103e165078bde447cf8d71d96d123d2cf
boonjun123654 <boonjunlee@gmail.com>
2025-08-02 23:42:26 +0800

    Update models.py

--- models.py
+++ models.py
@@ -9,5 +9,8 @@ class DrawResult(db.Model):
     code = db.Column(db.String(20), nullable=False)  # 例如 20250707/0010
     market = db.Column(db.String(1), nullable=False)  # M, K, T, ...
     head = db.Column(db.String(2), nullable=False)  # 头奖号码
-    specials = db.Column(db.String(20), nullable=False)  # 特别奖，例如 '55,46,87,99,10'
+    specials = db.Column(db.String(20), nullable=False)  # 特别奖
+    size_type = db.Column(db.String(2), nullable=False)   # 大 or 小
+    parity_type = db.Column(db.String(2), nullable=False) # 单 or 双
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    __table_args__ = (db.UniqueConstraint('code', 'market', name='_code_market_uc'),)
