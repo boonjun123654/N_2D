b0c42c68dcead8aca9e7abeb4578e6508d8a7f70
boonjun123654 <boonjunlee@gmail.com>
2025-09-21 16:55:59 +0800

    Update models.py

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class DrawResult(db.Model):
     __tablename__ = 'draw_results'
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(20), nullable=False)  # 例如 20250707/0010
-    market = db.Column(db.String(1), nullable=False)  # M, K, T, ...
+    market = db.Column(db.String(32), nullable=False)  # M, K, T, ...
     head = db.Column(db.String(2), nullable=False)  # 头奖号码
     specials = db.Column(db.String(20), nullable=False)  # 特别奖
     size_type = db.Column(db.String(2), nullable=False)   # 大 or 小
